51e879b5d50cde9dbb10f209a90e196b2dcb8038
MBriedis <mbriedis@gmail.com>
2015-01-30 13:20:50 +0200

    AbstractApiMethod

---
+++ src/Briedis/ApiBuilder/AbstractApiMethod.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace Briedis\ApiBuilder;
+
+
+abstract class AbstractApiMethod{
+	/**
+	 * Requests uri
+	 * @var string
+	 */
+	public $uri = '';
+
+	/**
+	 * Requests method
+	 * @var string
+	 */
+	public $method = 'GET';
+
+	/**
+	 * Descriptive title
+	 * @var string
+	 */
+	public $title = '';
+
+	/**
+	 * @var string
+	 */
+	public $description = '';
+
+	/**
+	 * Get structure(s) that will be passed to the request
+	 * @return StructureBuilder|StructureBuilder[]
+	 */
+	abstract public function getRequest();
+
+	/**
+	 * Get structure(s) that will be returned by the request
+	 * @return StructureBuilder|StructureBuilder[]
+	 */
+	abstract public function getResponse();
+}
