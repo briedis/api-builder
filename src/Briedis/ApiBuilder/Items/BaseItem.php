1daff9a50ee87577f407e62403d169e6105cd2b6
MBriedis <mbriedis@gmail.com>
2015-02-01 19:03:06 +0200

    Clean-up, comments

--- src/Briedis/ApiBuilder/Items/BaseItem.php
+++ src/Briedis/ApiBuilder/Items/BaseItem.php
@@ -7,26 +7,46 @@ namespace Briedis\ApiBuilder\Items;
 class BaseItem{
 	const TYPE = 'mixed';
 
+	/**
+	 * Name of the parameter
+	 * @var string
+	 */
 	public $name = '';
 
+	/**
+	 * Description for this parameter
+	 * @var string
+	 */
 	public $description = '';
 
+	/**
+	 * Is optional. All parameters are required by default
+	 * @var bool
+	 */
 	public $isOptional = false;
 
+	/**
+	 * Is type an array
+	 * @var bool
+	 */
 	public $isArray = false;
 
 	/**
+	 * Is enumerable type, has a list of valid values
 	 * @var bool
 	 */
 	public $isEnum = false;
 
 	/**
+	 * List of valid values. Is used only if isEnum is set
 	 * @var array
 	 */
 	public $enumValues = [];
 
-	public $parameters;
-
+	/**
+	 * @param string $name Parameters name
+	 * @param string $description
+	 */
 	public function __construct($name = '', $description = ''){
 		$this->name = $name;
 		$this->description = $description;
